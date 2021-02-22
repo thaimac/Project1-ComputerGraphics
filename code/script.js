56e8f441cddde62ce511d992437d21cbcfbba45a
unknown <tmcewen4@gmail.com>
2021-02-21 21:09:35 -0500

    Add circle to canvas

--- code/script.js
+++ code/script.js
@@ -1,3 +1,16 @@
+// Vertex shader program
+var VSHADER_SOURCE =
+  'attribute vec4 a_Position;\n' +
+  'void main() {\n' +
+  '  gl_Position = a_Position;\n' +
+  '}\n';
+
+// Fragment shader program
+var FSHADER_SOURCE =
+  'void main() {\n' +
+  '  gl_FragColor = vec4(1.0, 0.4, 0.0, 1.0);\n' +
+  '}\n';
+
 function main() {
 	//Retrieve canvas element
 	var canvas = document.getElementById('webgl');
@@ -9,9 +22,60 @@ function main() {
 		return false;
 	}
 
+	// Initialize shaders
+	if (!initShaders(gl, VSHADER_SOURCE, FSHADER_SOURCE)) {
+		console.log('Failed to intialize shaders.');
+		return;
+	}
+
+	// Write the positions of vertices to a vertex shader
+	var n = initVertexBuffer(gl);
+	if (n < 0) {
+	  console.log('Failed to set the positions of the vertices');
+	  return;
+	}
+
 	//Set clear color
 	gl.clearColor(0.0,0.0,0.0,1.0);
 
 	//Clear Canvas
 	gl.clear(gl.COLOR_BUFFER_BIT);
+
+	// Draw the rectangle
+	gl.drawArrays(gl.TRIANGLE_STRIP, 0, n);
 }
+
+function initVertexBuffer(gl) {
+    // Create a buffer object
+    var vertexBuffer = gl.createBuffer(),
+        vertices = [],
+        vertCount = 2;
+    for (var i=0.0; i<=360; i+=1) {
+      // degrees to radians
+      var j = i * Math.PI / 180;
+      // X Y Z
+      var vert1 = [
+        Math.sin(j)*0.5,
+        Math.cos(j)*0.5,
+      ];
+      var vert2 = [
+        0,
+        0,
+      ];
+      vertices = vertices.concat(vert1);
+      vertices = vertices.concat(vert2);
+    }
+    var n = vertices.length / vertCount;
+    gl.bindBuffer(gl.ARRAY_BUFFER, vertexBuffer);
+    gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(vertices), gl.STATIC_DRAW);
+
+    var a_Position = gl.getAttribLocation(gl.program, 'a_Position');
+	if (a_Position < 0) {
+		console.log('Failed to get the storage location of a_Position');
+		return -1;
+	  }
+    gl.enableVertexAttribArray(a_Position);
+    gl.vertexAttribPointer(a_Position, vertCount, gl.FLOAT, false, 0, 0);
+
+    return n;
+  }
