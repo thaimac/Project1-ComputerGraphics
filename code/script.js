1f55563b0f303fa9869d3cbe5370d62c116747ed
unknown <tmcewen4@gmail.com>
2021-02-28 19:10:33 -0500

    Modify draw circle. Implement bateria spawn.

--- code/script.js
+++ code/script.js
@@ -7,75 +7,129 @@ var VSHADER_SOURCE =
 
 // Fragment shader program
 var FSHADER_SOURCE =
+  'precision mediump float;\n' +
+  'uniform vec4 FragColor;\n' +
   'void main() {\n' +
-  '  gl_FragColor = vec4(1.0, 0.4, 0.0, 1.0);\n' +
+  '  gl_FragColor = FragColor;\n' +
   '}\n';
 
-function main() {
-	//Retrieve canvas element
-	var canvas = document.getElementById('webgl');
+var main = function() {
 
-	//Render context
-	var gl = getWebGLContext(canvas);
+	var currBacNum = 0;
+	var totalBacteria = 5; 
+	var bacteria = [];
+
+	//Create main canvas
+	var canvas = document.getElementById('webgl');
+	//Render context for main canvas
+	var gl = canvas.getContext('webgl');
 	if(!gl) {
 		console.log("Failed to render this context");
 		return false;
 	}
 
-	// Initialize shaders
-	if (!initShaders(gl, VSHADER_SOURCE, FSHADER_SOURCE)) {
-		console.log('Failed to intialize shaders.');
-		return;
-	}
+  //Create buffer to bind vertex data to
+  var vertexBuffer = gl.createBuffer();
+  gl.bindBuffer(gl.ARRAY_BUFFER, vertexBuffer);
 
-	// Write the positions of vertices to a vertex shader
-	var n = initVertexBuffer(gl);
-	if (n < 0) {
-	  console.log('Failed to set the positions of the vertices');
-	  return;
-	}
-
-	//Set clear color
-	gl.clearColor(0.0,0.0,0.0,1.0);
-
-	//Clear Canvas
-	gl.clear(gl.COLOR_BUFFER_BIT);
-
-	// Draw the rectangle
-	gl.drawArrays(gl.TRIANGLE_STRIP, 0, n);
-}
-
-function initVertexBuffer(gl) {
-    // Create a buffer object
-    var vertexBuffer = gl.createBuffer(),
-        vertices = [],
-        vertCount = 2;
-    for (var i=0.0; i<=360; i+=1) {
-      // degrees to radians
-      var j = i * Math.PI / 180;
-      // X Y Z
-      var vert1 = [
-        Math.sin(j)*0.5,
-        Math.cos(j)*0.5,
-      ];
-      var vert2 = [
-        0,
-        0,
-      ];
-      vertices = vertices.concat(vert1);
-      vertices = vertices.concat(vert2);
-    }
-    var n = vertices.length / vertCount;
-    gl.bindBuffer(gl.ARRAY_BUFFER, vertexBuffer);
-    gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(vertices), gl.STATIC_DRAW);
-
-    var a_Position = gl.getAttribLocation(gl.program, 'a_Position');
-	if (a_Position < 0) {
-		console.log('Failed to get the storage location of a_Position');
-		return -1;
-	  }
-    gl.enableVertexAttribArray(a_Position);
-    gl.vertexAttribPointer(a_Position, vertCount, gl.FLOAT, false, 0, 0);
-
-    return n;
+  //Initialize shaders and handle error
+  if(!initShaders(gl, VSHADER_SOURCE, FSHADER_SOURCE)) {
+    console.log("Failed to initialize shaders.");
   }
+
+  var coordinates = gl.getAttribLocation(gl.program, "a_Position");
+  var fragColor = gl.getUniformLocation(gl.program, "FragColor");
+
+  gl.vertexAttribPointer(coordinates, 3, gl.FLOAT, false, 0, 0);
+	gl.enableVertexAttribArray(coordinates);
+
+  //function used to draw main disk and all bacteria that will grow on it 
+  function drawCircle(gl,x,y,r,color, fragColor) {
+    var points = [];
+    for (var i=0.0; i<=360; i+=1) {
+      var y1 = r*Math.sin(i)+y;
+			var x1 = r*Math.cos(i)+x;
+
+			var y2 = r*Math.sin(i+1)+y;
+			var x2 = r*Math.cos(i+1)+x;
+
+			points.push(x);
+			points.push(y);
+			points.push(0);
+
+			points.push(x1);
+			points.push(y1);
+			points.push(0);
+
+			points.push(x2);
+			points.push(y2);
+			points.push(0);
+    }
+
+    gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(points), gl.STATIC_DRAW);
+
+    gl.uniform4f(fragColor, color[0], color[1], color[2], color[3]);
+    gl.clearColor(0, 1, 0, 0.9);
+    gl.drawArrays(gl.TRIANGLE_STRIP, 0, 360*3);
+  }
+
+  //Function to randomly choose quadrant that bacteria spawns in
+	function selectQuadrant(n){
+    n = Math.random() >= 0.5 ? n*(-1) : n;
+		return n;
+	}
+
+  //Class to hold data about each individual bacteria
+	class Bacteria {
+
+    //spawns bacteria
+		spawn() {
+			this.randomCoordinates();
+      this.x = this.randomX*Math.sin(this.angle);
+			this.y = this.randomY*Math.cos(this.angle);
+			this.radius = 0.04;
+			this.color = [Math.random(), Math.random(), Math.random(), 1];
+			this.alive = true;
+			currBacNum++;
+		}
+
+		update() {
+      //Losing condition
+      if(this.radius > 0.3) {
+        console.log("YOU LOSE");
+        this.alive = false;
+        bacteria.splice(this,1);
+      }
+			//Increase the size of each bacteria by 'radius' at each tick. Adjust to modify difficulty 
+			this.radius += 0.0003;
+			//Grow bacteria
+		  drawCircle(gl, this.x, this.y, this.radius, this.color, fragColor);
+		}
+
+		//Function that randomly selects the coordinates of a spawning bacteria
+		randomCoordinates() {
+			this.angle = Math.random();
+			this.randomX = selectQuadrant(0.8);
+			this.randomY = selectQuadrant(0.8);
+		}
+	} 
+
+  // Create and push new Bacteria objects into bacArr, then spawn each Bacteria
+	for(var i = 0; i<totalBacteria; i++){
+		bacteria.push(new Bacteria());
+		bacteria[i].spawn();
+	}
+
+  //Function that carries out the gameplay
+	function play() {
+			for (let i in bacteria) {
+					bacteria[i].update();
+				}
+		//create main game circle in centre of canvas
+		drawCircle(gl, 0,0,0.8,[0.05, 0.1, 0.05, 0.5], fragColor);
+		requestAnimationFrame(play);
+	}
+
+  //Start game
+	play();
+}
